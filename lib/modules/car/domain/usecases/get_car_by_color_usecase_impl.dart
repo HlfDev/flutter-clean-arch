7742ccfd162200895d46b53998651d6acfc2c461
Pedro Henrique <pedrohlf.contato@gmail.com>
2022-01-15 22:11:56 -0300

    add GetCarByColorUsecase tests

--- lib/modules/car/domain/usecases/get_car_by_color_usecase_impl.dart
+++ lib/modules/car/domain/usecases/get_car_by_color_usecase_impl.dart
@@ -1,10 +1,10 @@
 import 'package:clean_arch/modules/car/domain/entities/car_entity.dart';
 import 'package:clean_arch/modules/car/domain/usecases/usecases.dart';
 
-class GetCarByColorUsecaseImlp implements GetCarByColorUsecase {
+class GetCarByColorUsecaseImpl implements GetCarByColorUsecase {
   @override
   CarEntity call(String color) {
-    if (color.toUpperCase() == 'VERMELHO') {
+    if (color.toUpperCase() == 'RED') {
       return CarEntity(licensePlate: 'ABC100', doorNumbers: 4, value: 40000);
     }
     return CarEntity(licensePlate: 'ABC200', doorNumbers: 2, value: 20000);
---
+++ test/modules/car/domain/usecases/get_car_by_color_usecase_test.dart
@@ -0,0 +1,42 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:clean_arch/modules/car/domain/entities/entities.dart';
+import 'package:clean_arch/modules/car/domain/usecases/usecases.dart';
+
+void main() {
+  late GetCarByColorUsecaseImpl getCarByColorUsecase;
+
+  setUp(() {
+    getCarByColorUsecase = GetCarByColorUsecaseImpl();
+  });
+
+  test('Should show a CarEntity when GetCarByColorUsecase is called', () {
+    // Act
+    final result = getCarByColorUsecase('Red');
+
+    // Assert
+    expect(result, isInstanceOf<CarEntity>());
+  });
+
+  test('Should show a CarEntity with four doors when color is red', () {
+    // Arrange
+    const expectValue = 4;
+
+    // Act
+    final result = getCarByColorUsecase('Red');
+
+    // Assert
+    expect(result.doorNumbers, expectValue);
+  });
+
+  test('Should show a CarEntity with two doors when color is different than red', () {
+    // Arrange
+    const expectValue = 2;
+
+    // Act
+    final result = getCarByColorUsecase('Blue');
+
+    // Assert
+    expect(result.doorNumbers, expectValue);
+  });
+}
